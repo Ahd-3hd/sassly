9556657a7de6df2cd91f07c6284cf5d7f810db3f
ahd <ahdhani0@gmail.com>
2020-12-05 16:37:21 +0300

    structure pages and components, install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <div>hehgeheh</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/utils/theme.ts
@@ -0,0 +1,27 @@
+const colors = {
+  primary: "#0C0C0C",
+  grey1: "#343434",
+  gray2: "#626262",
+  gray3: "#9C9C9C",
+  gray4: "#D2D2D2",
+  gray5: "#F5F5F5",
+  gray6: "#FBFBFB",
+  white: "#FFFFFF",
+};
+
+const fontSizes = {
+  heading1: "68px",
+  heading2: "42px",
+  heading3: "32px",
+  heading4: "26px",
+  heading5: "22px",
+  heading6: "18px",
+  paragraphSmall: "16px",
+  paragraphDefault: "18px",
+  paragraphLarge: "22px",
+};
+
+export default {
+  colors,
+  fontSizes,
+};
