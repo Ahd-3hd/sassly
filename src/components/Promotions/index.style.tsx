c9189dc7f0c7039798d69c4a8e9e1d178188da6c
ahd <ahdhani0@gmail.com>
2020-12-06 17:35:44 +0300

    finish promotions

--- src/components/Promotions/index.style.tsx
+++ src/components/Promotions/index.style.tsx
@@ -11,6 +11,7 @@ export const TitleContainer = styled.div`
   > h3 {
     max-width: 30ch;
     font-size: ${({ theme: { fontSizes } }) => fontSizes.heading2};
+    word-break: keep-all;
   }
   @media (min-width: 900px) {
     display: flex;
@@ -18,3 +19,64 @@ export const TitleContainer = styled.div`
     align-items: flex-start;
   }
 `;
+
+export const CardsContainer = styled.div`
+  @media (min-width: 480px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    background: ${({ theme: { colors } }) => colors.grey4};
+    align-items: start;
+    grid-gap: 1px;
+  }
+  @media (min-width: 900px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
+export const PromoCard = styled.div`
+  padding: 1rem;
+  background: #fbfbfb;
+  height: 330px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-bottom: 1px solid ${({ theme: { colors } }) => colors.grey4};
+  @media (min-width: 480px) {
+    border-bottom: none;
+    align-items: center;
+    text-align: center;
+  }
+}
+  @media (min-width: 900px) {
+    height: 300px;
+  }
+`;
+export const CardIcon = styled.img`
+  border-radius: 100%;
+  max-width: 80px;
+`;
+export const CardTitle = styled.p`
+  color: ${({ theme: { colors } }) => colors.primary};
+  font-weight: bold;
+  font-size: 18px;
+`;
+export const CardParagraph = styled.p`
+  color: ${({ theme: { colors } }) => colors.grey2};
+  letter-spacing: 0.8px;
+  line-height: 1.6rem;
+`;
+
+export const Promo = ({
+  icon,
+  title,
+  text,
+}: {
+  icon: string;
+  title: string;
+  text: string;
+}) => (
+  <PromoCard>
+    <CardIcon src={icon} alt="icon" />
+    <CardTitle>{title}</CardTitle>
+    <CardParagraph>{text}</CardParagraph>
+  </PromoCard>
+);
--- src/components/Promotions/index.tsx
+++ src/components/Promotions/index.tsx
@@ -1,5 +1,49 @@
-import { Wrapper, TitleContainer } from "./index.style";
+import { Wrapper, TitleContainer, CardsContainer, Promo } from "./index.style";
 import { Heading3, Paragraph } from "../Typography";
+import aiIcon from "../../assets/icons/ai.svg";
+import bookIcon from "../../assets/icons/book.svg";
+import cloudIcon from "../../assets/icons/cloud.svg";
+import lockIcon from "../../assets/icons/lock.svg";
+import phoneIcon from "../../assets/icons/phone.svg";
+import textIcon from "../../assets/icons/text.svg";
+const data = [
+  {
+    icon: textIcon,
+    title: "Simple, yet powerful",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing eliterlore oleri. Ullamcorper auctor volutpat nulla.",
+  },
+  {
+    icon: aiIcon,
+    title: "Built-in AI features",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing eliterlore oleri. Ullamcorper auctor volutpat nulla.",
+  },
+  {
+    icon: phoneIcon,
+    title: "Real time sync",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing eliterlore oleri. Ullamcorper auctor volutpat nulla.",
+  },
+  {
+    icon: bookIcon,
+    title: "Easy notes organization",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing eliterlore oleri. Ullamcorper auctor volutpat nulla.",
+  },
+  {
+    icon: lockIcon,
+    title: "Advanced 256-bit encryption",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing eliterlore oleri. Ullamcorper auctor volutpat nulla.",
+  },
+  {
+    icon: cloudIcon,
+    title: "Cloud based",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing eliterlore oleri. Ullamcorper auctor volutpat nulla.",
+  },
+];
 
 const Promotions = () => {
   return (
@@ -11,6 +55,16 @@ const Promotions = () => {
           condimentum at quam tellus non.
         </Paragraph>
       </TitleContainer>
+      <CardsContainer>
+        {data.map((item, index) => (
+          <Promo
+            key={index}
+            title={item.title}
+            text={item.text}
+            icon={item.icon}
+          />
+        ))}
+      </CardsContainer>
     </Wrapper>
   );
 };
